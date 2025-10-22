dd22af0a237c7eb2322260de6d142d609d25b615
Waheedullah Azizi <abdulwaheeddanishjo@gmail.com>
2025-10-22 15:36:14 +0430

    Two Way Data binging

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
 export class User {
   name: string = 'Ahmad';
   email: string = 'Ahmad@gmail.com';
+  random = '';
   onSave() {
     alert('User saved successfully!');
   }
